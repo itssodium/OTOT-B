218cea0b9ee982b3a6565bf2536a9b46cb79be72
itssodium <noorulazlina@gmail.com>
2022-10-23 21:09:54 +0800

    Task B1 and B2

--- index.js
+++ index.js
@@ -16,18 +16,24 @@ app.get('/get', (req, res) => {
 app.post('/post', (req, res) => {
     const name = req.body.name;
     const role = req.body.role;
+    if (name == null || role == null) {
+        return res.status(400).send("Please fill up name and role")
+    }
     const isPresent = users.some(user => user.name === name)
     if (isPresent) {
-        res.status(200).send(`${name} already exists`)
+        res.status(208).send(`${name} already exists`)
     } else {
         users.push({name: name, role: role})
-        res.status(200).send(`${name} added`)
+        res.status(201).send(`${name} added`)
     }
 })
 
 app.put('/put', (req, res) => {
     const name = req.body.name;
     const role = req.body.role;
+    if (name == null || role == null) {
+        return res.status(400).send("Please fill up name and role")
+    }
     const isPresent = users.some(user => user.name === name)
     if (isPresent) {
         users.forEach(user => {
@@ -37,18 +43,21 @@ app.put('/put', (req, res) => {
         })
         res.status(200).send(`${name} role is changed`)
     } else {
-        res.status(200).send(`${name} is not present, use POST to add user`)
+        res.status(204).send(`${name} is not present, use POST to add user`)
     }
 })
 
 app.delete('/delete', (req, res) => {
     const name = req.body.name;
+    if (name == null) {
+        return res.status(400).send("Please fill up name")
+    }
     const isPresent = users.some(user => user.name === name)
     if (isPresent) {
         users = users.filter(user => user.name != name)
         res.status(200).send(`${name} is deleted`)
     } else {
-        res.status(200).send(`${name} does not exist`)
+        res.status(405).send(`${name} does not exist`)
     }
     
 })
--- tests/test.js
+++ tests/test.js
@@ -12,7 +12,138 @@ describe('API testing', () => {
             .get('/get')
             .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
+                done();
+            })
+        })
+    })
+
+    describe("POST", () => {
+        it("should add user", (done) => {
+            chai.request(app)
+            .post('/post')
+            .send({name: 'user2', role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(201);
+                done();
+            })
+        })
+        it("should not add user", (done) => {
+            chai.request(app)
+            .post('/post')
+            .send({name: 'user2', role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(208);
+                done();
+            })
+        })
+        it("empty name and role should give error", (done) => {
+            chai.request(app)
+            .post('/post')
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+        it("empty name gives error", (done) => {
+            chai.request(app)
+            .post('/post')
+            .send({role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+        it("empty role gives error", (done) => {
+            chai.request(app)
+            .post('/post')
+            .send({name: 'user3'})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+    })
+
+    describe("PUT", () => {
+        it("should update user", (done) => {
+            chai.request(app)
+            .put('/put')
+            .send({name: 'user1', role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        })
+        it("should not dot do anything", (done) => {
+            chai.request(app)
+            .put('/put')
+            .send({name: 'user3', role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(204);
+                done();
+            })
+        })
+        it("empty name and role should give error", (done) => {
+            chai.request(app)
+            .put('/put')
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+        it("empty name gives error", (done) => {
+            chai.request(app)
+            .put('/put')
+            .send({role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+        it("empty role gives error", (done) => {
+            chai.request(app)
+            .put('/put')
+            .send({name: 'user1'})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        })
+    })
+
+    describe("DELETE", () => {
+        it("should delete user", (done) => {
+            chai.request(app)
+            .delete('/delete')
+            .send({name: 'user1', role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        })
+        it("should delete user", (done) => {
+            chai.request(app)
+            .delete('/delete')
+            .send({name: 'user2', role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        })
+        it("should delete user", (done) => {
+            chai.request(app)
+            .delete('/delete')
+            .send({name: 'user1', role: 'viewer'})
+            .end((err, res) => {
+                res.should.have.status(405);
+                done();
+            })
+        })
+        it("empty name should give error", (done) => {
+            chai.request(app)
+            .delete('/delete')
+            .end((err, res) => {
+                res.should.have.status(400);
                 done();
             })
         })

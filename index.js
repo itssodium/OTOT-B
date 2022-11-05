e7d9c082d55562938a423ddfa8515f9b9ef14f5f
itssodium <noorulazlina@gmail.com>
2022-11-05 23:50:31 +0800

    fix client null problem

--- index.js
+++ index.js
@@ -48,7 +48,66 @@ connect().then(() => {
             });
             res.status(200).send(`Users are ${names.toString()}`);
         }
-        
+    })
+
+    app.post('/post', async (req, res) => {
+        const name = req.body.name;
+        const role = req.body.role;
+        if (name == null || role == null || name.length === 0 || role.length === 0) {
+            return res.status(400).send("Please fill up name and role")
+        }
+    
+        const db = await client.db();
+        const collection = db.collection('users');
+        const users_mongo = await collection.find().toArray();
+        const names_mongo = users_mongo.map(user => user.name);
+        const isPresent = names_mongo.includes(name);
+    
+        if (isPresent) {
+            res.status(208).send(`${name} already exists, use put for updates`)
+        } else {
+            const data = {name: name, role: role};
+            collection.insertOne(data);
+            res.status(201).send(`${name} added`)
+        }
+    })
+
+    app.put('/put', async (req, res) => {
+        const name = req.body.name;
+        const role = req.body.role;
+        if (name == null || role == null || name.length === 0 || role.length === 0) {
+            return res.status(400).send("Please fill up name and role")
+        }
+    
+        const db = await client.db();
+        const collection = db.collection('users');
+        const users_mongo = await collection.find().toArray();
+        const names_mongo = users_mongo.map(user => user.name);
+        const isPresent = names_mongo.includes(name);
+    
+        if (isPresent) {
+            const change = {name: name};
+            const data = {name: name, role: role};
+            collection.replaceOne(change, data).then((value) => {
+                res.status(200).send(`${name} role is changed`);
+            });
+        } else {
+            res.status(404).send(`${name} is not present, use POST to add user`);
+        }
+    })
+
+    app.delete('/delete', async (req, res) => {
+        const db = await client.db();
+        const collection = db.collection('users');
+        const users_mongo = await collection.find().toArray();
+        const isEmpty = users_mongo.length === 0
+        if (isEmpty) {
+            res.status(204).send('no users to delete');
+        } else {
+            collection.remove().then((value) => {
+                res.status(200).send(`users are deleted`);
+            });
+        }
     })
 });
 
@@ -69,9 +128,9 @@ connect().then(() => {
         res.status(200).send(`Users are ${names.toString()}`);
     }
     
-})*/
+})
 
-app.post('/post', async (req, res) => {
+/*app.post('/post', async (req, res) => {
     const name = req.body.name;
     const role = req.body.role;
     if (name == null || role == null || name.length === 0 || role.length === 0) {
@@ -118,18 +177,7 @@ app.put('/put', async (req, res) => {
 })
 
 app.delete('/delete', async (req, res) => {
-    client.db().collection('users').find().toArray().then(arr => {
-        const isEmpty = arr.length === 0
-        if (isEmpty) {
-            res.status(204).send('no users to delete');
-        } else {
-            collection.remove().then((value) => {
-                res.status(200).send(`users are deleted`);
-            });
-        }
-    });
-
-    /*const db = await client.db();
+    const db = await client.db();
     const collection = db.collection('users');
     const users_mongo = await collection.find().toArray();
     const isEmpty = users_mongo.length === 0
@@ -139,8 +187,8 @@ app.delete('/delete', async (req, res) => {
         collection.remove().then((value) => {
             res.status(200).send(`users are deleted`);
         });
-    }*/
-})
+    }
+})*/
 
 app.listen(process.env.PORT || 5000)
 module.exports = app; 

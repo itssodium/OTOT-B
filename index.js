8c2041c00072c1b9c9e6f12f4e3dc93582fbeeba
itssodium <noorulazlina@gmail.com>
2022-11-06 00:11:37 +0800

    fix client null problem

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(cors(corsOptions));
 app.options('*', cors())
 
 var client;
-var collection;
 
 async function connect() {
     client = await MongoClient.connect('mongodb://localhost:27017/');
@@ -22,18 +21,21 @@ async function connect() {
     const containsUsers = collections.map(c => c.name).includes('users');
     if (containsUsers) {
         const db = await client.db();
-        collection = db.collection('users');
         console.log('contains')
     } else {
         const db = await client.db();
         db.createCollection('users');
-        collection = db.collection('users');
         console.log('created')
     }
 }
 
 connect().then(() => {
-    app.get('/get', async (req, res) => {
+    console.log('connected');
+});
+
+//gcloud app deploy would give the link to frontend
+app.get('/get', async (req, res) => {
+    connect().then(async () => {
         const db = client.db();
         const collection = db.collection('users');
         const users_mongo = await collection.find().toArray();
@@ -48,89 +50,10 @@ connect().then(() => {
             });
             res.status(200).send(`Users are ${names.toString()}`);
         }
-    })
-
-    app.post('/post', async (req, res) => {
-        const name = req.body.name;
-        const role = req.body.role;
-        if (name == null || role == null || name.length === 0 || role.length === 0) {
-            return res.status(400).send("Please fill up name and role")
-        }
-    
-        const db = await client.db();
-        const collection = db.collection('users');
-        const users_mongo = await collection.find().toArray();
-        const names_mongo = users_mongo.map(user => user.name);
-        const isPresent = names_mongo.includes(name);
-    
-        if (isPresent) {
-            res.status(208).send(`${name} already exists, use put for updates`)
-        } else {
-            const data = {name: name, role: role};
-            collection.insertOne(data);
-            res.status(201).send(`${name} added`)
-        }
-    })
-
-    app.put('/put', async (req, res) => {
-        const name = req.body.name;
-        const role = req.body.role;
-        if (name == null || role == null || name.length === 0 || role.length === 0) {
-            return res.status(400).send("Please fill up name and role")
-        }
-    
-        const db = await client.db();
-        const collection = db.collection('users');
-        const users_mongo = await collection.find().toArray();
-        const names_mongo = users_mongo.map(user => user.name);
-        const isPresent = names_mongo.includes(name);
-    
-        if (isPresent) {
-            const change = {name: name};
-            const data = {name: name, role: role};
-            collection.replaceOne(change, data).then((value) => {
-                res.status(200).send(`${name} role is changed`);
-            });
-        } else {
-            res.status(404).send(`${name} is not present, use POST to add user`);
-        }
-    })
-
-    app.delete('/delete', async (req, res) => {
-        const db = await client.db();
-        const collection = db.collection('users');
-        const users_mongo = await collection.find().toArray();
-        const isEmpty = users_mongo.length === 0
-        if (isEmpty) {
-            res.status(204).send('no users to delete');
-        } else {
-            collection.remove().then((value) => {
-                res.status(200).send(`users are deleted`);
-            });
-        }
-    })
-});
-
-//gcloud app deploy would give the link to frontend
-/*app.get('/get', async (req, res) => {
-    const db = client.db();
-    const collection = db.collection('users');
-    const users_mongo = await collection.find().toArray();
-    const isEmpty = users_mongo.length === 0
-    console.log(isEmpty);
-    if (isEmpty) {
-        res.status(204).send('no users present');
-    } else {
-        const users_mongo = await collection.find().toArray();
-        const names = users_mongo.map(user => {
-            return `Name: ${user.name}, Role: ${user.role}`
-        });
-        res.status(200).send(`Users are ${names.toString()}`);
-    }
-    
+    });
 })
 
-/*app.post('/post', async (req, res) => {
+app.post('/post', async (req, res) => {
     const name = req.body.name;
     const role = req.body.role;
     if (name == null || role == null || name.length === 0 || role.length === 0) {
@@ -188,7 +111,7 @@ app.delete('/delete', async (req, res) => {
             res.status(200).send(`users are deleted`);
         });
     }
-})*/
+})
 
 app.listen(process.env.PORT || 5000)
 module.exports = app; 
